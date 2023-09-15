141162b26c5826395e216246c8fc4491e7549c65
Eder Aguilar <loboeder@yahoo.com>
2023-09-14 22:21:50 -0400

    Fixed Frequencies header to have a class declaration. Prefixed methods with class name and scope resolution operator in Frequencies implementation file. Added double quotes to the #include directive for Frequencies.h. Declared a frequencies object to call its functions.

--- Hw1/Frequencies.h
+++ Hw1/Frequencies.h
@@ -1,20 +1,24 @@
-#ifndef LETTERS
-#define LETTERS
+#ifndef Frequencies_h
+#define Frequencies_h
 #include <fstream>
 
-/*
- * Author: Ally Oh
- * Function letterFreq combs through an open text file with flag std::ios::in, keeps a count
- * of how often each character occurs and how many letters there are total, and calculates their frequencies.
- * It returns a pointer to a float array of size 26 containing the frequencies for each letter in
- * alphabetical order.
- */
-float * const letterFreq(std::fstream&);
+class Frequencies {
+    
+public:
+    /*
+     * Author: Ally Oh
+     * Function letterFreq combs through an open text file with flag std::ios::in, keeps a count
+     * of how often each character occurs and how many letters there are total, and calculates their frequencies.
+     * It returns a pointer to a float array of size 26 containing the frequencies for each letter in
+     * alphabetical order.
+     */
+    float * const letterFreq(std::fstream&);
 
-/*
- * Author: Ally Oh
- * Create a nicely formatted string to be used to display letter frequencies.
- */
-std::string const letFreqString(float[], std::string);
+    /*
+     * Author: Ally Oh
+     * Create a nicely formatted string to be used to display letter frequencies.
+     */
+    std::string const letFreqString(float[], std::string);
+};
 
 #endif
